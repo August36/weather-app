fc204d3e29fc2f8761503bd71732bbac1c5ce054
August <augustjp@live.dk>
2025-03-17 18:56:47 +0100

    Render weather data using template

--- main.js
+++ main.js
@@ -1,17 +1,43 @@
 async function weatherCph() {
-    const url = "https://api.open-meteo.com/v1/forecast?latitude=55.6761&longitude=12.5683&current_weather=true";
+    const url = "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&daily=weather_code,rain_sum,sunshine_duration,sunrise,temperature_2m_max,temperature_2m_min,wind_speed_10m_max&hourly=temperature_2m";
     try {
         const response = await fetch(url);
         if (!response.ok) {
             throw new Error(`response statuses: ${response.status}`);
         }
-        const json = await response.json();
-        console.log(json);
+        const data = await response.json();
+        console.log(data);
 
-    } catch {
+        const dates = data.daily.time
+        const temperature = data.daily.temperature_2m_max
+        const weathercode = data.daily.weather_code
+        const rain = data.daily.rain_sum;
+        const sunrise = data.daily.sunrise;
+
+        const container = document.querySelector("#weather_container");
+        const template = document.querySelector("#weather_card");
+
+        for (let i = 0; i < dates.length; i++) {
+            const card = template.content.cloneNode(true);
+
+            card.querySelector(".date").textContent = `Dato ${dates[i]}`
+            card.querySelector(".temperature").textContent = `Maks temp: ${temperature[i]} °C`
+            card.querySelector(".weathercode").textContent = `Weather code: ${weathercode[i]}`
+            card.querySelector(".rain").textContent = `Regn: ${rain[i]} mm`
+            card.querySelector(".sunrise").textContent = `Sunrise ${sunrise[i]}`
+
+            container.appendChild(card)
+        }
+
+    } catch (error) {
         console.error(error.message)
     }
     
 }
+
+//Næste opgave:
+// Tilføj mulighed for at søge efter en by og vis vejret for byen
+// searchparams?
+// brug Nominatim API til at konvertere bynavne til kordinater
 
 weatherCph()
